eadc7b46a2d72fa1708de2ab4e491b8c8800096d
thenav56 <ayernavin@gmail.com>
2024-08-06 19:24:46 +0545

    Add TODO move and DOING clone command
    - Fix test runner

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -29,6 +29,7 @@ class UserAdmin(DjangoUserAdmin):
                     "first_name",
                     "last_name",
                     "department",
+                    "display_picture",
                 )
             },
         ),
