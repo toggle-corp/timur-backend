18ad81af038104b71d36f8459273fab2191426ef
thenav56 <ayernavin@gmail.com>
2025-04-16 23:32:13 +0545

    fix(django): Ignore csrf_exempt for graphql
    - Add uwsgi config for deployment

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
 
 from apps.common.views import dev_sign_in, google_oauth
 from main.graphql.schema import CustomAsyncGraphQLView
@@ -18,9 +19,12 @@ urlpatterns = [
     # path('health-check/', include('health_check.urls')),
     path(
         "graphql/",
-        CustomAsyncGraphQLView.as_view(
-            schema=graphql_schema,
-            graphql_ide=False,
+        # TODO: Remove this after updating the frontend to send csrf tokens
+        csrf_exempt(
+            CustomAsyncGraphQLView.as_view(
+                schema=graphql_schema,
+                graphql_ide=False,
+            ),
         ),
         name="graphql",
     ),
