03cd140ebcff6d56db6c4369ce5a71ecd03ff76c
thenav56 <ayernavin@gmail.com>
2025-05-01 17:15:22 +0545

    feat(standup): add DailyUserStandup model and Slack-related user fields

--- apps/standup/models.py
+++ apps/standup/models.py
@@ -1,19 +1,58 @@
+import typing
+
 from django.db import models
+from django.db.models.functions import Now
+from django.utils.translation import gettext_lazy as _
 
 from apps.common.models import UserResource
-
-# from apps.user.models import User
-
-
-# class DailyUserStandup(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     date = models.DateField()
-
-#     slack_thread_id = models.CharField(max_length=200)  # TODO: Check length
-#     text = models.TextField()  # TODO: Do we need this?
+from apps.user.models import User
 
 
 class Quote(UserResource):
     text = models.TextField()
     author = models.CharField(max_length=225)
     last_viewed = models.DateTimeField(null=True, blank=True)
+
+    @classmethod
+    def get_random_quote(cls, track_last_viewed=False) -> typing.Self | None:
+        quote = cls.objects.order_by(
+            models.F("last_viewed").asc(nulls_first=True),
+        ).first()
+        if quote and track_last_viewed:
+            cls.objects.filter(pk=quote.pk).update(last_viewed=Now())
+        return quote
+
+
+# TODO: Add created_at, created_by, modified_by, modified_at
+class DailyUserStandup(models.Model):
+    date = models.DateField(unique=True)
+
+    quote = models.ForeignKey(Quote, on_delete=models.SET_NULL, null=True, blank=True)
+
+    conductor = models.ForeignKey(
+        User,
+        help_text=_("User responsible for conducting the stand-up"),
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    fallback_conductor = models.ForeignKey(
+        User,
+        help_text=_("Fallback user when conductor is not available"),
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    slack_thread_ts = models.CharField(max_length=20, null=True, blank=True)
+
+    # typing hints
+    quote_id: int | None
+    conductor_id: int | None
+    fallback_conductor_id: int | None
+
+    @typing.override
+    def __str__(self):
+        return str(self.date)
