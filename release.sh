7b01ddb2b08ed8d63c08468c50863de3254fba3d
thenav56 <ayernavin@gmail.com>
2026-04-07 20:43:33 +0545

    feat: integrate with banjo-action(+fugit) for github action

--- release.sh
+++ release.sh
@@ -1,77 +1,30 @@
-#!/usr/bin/env bash
-# Original https://github.com/orhun/git-cliff/blob/main/release.sh
-set -e
+#!/bin/bash
 
-# Update this to archive old changelogs
-# NOTE: Make sure to also update cliff.toml:footer to includes those archvied changelogs as well
-START_COMMIT="ed944248282e099601ce15b4903fa6345c0b7845"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SCRIPT_DIR
 
-if ! command -v typos &>/dev/null; then
-  echo "typos is not installed. Run 'cargo install typos-cli' to install it, otherwise the typos won't be fixed"
-fi
+function release_custom_hook {
+    # shellcheck disable=SC2154
+    echo "Running custom hook for ${version_tag}"
 
-if ! command -v semver &>/dev/null; then
-  echo "semver is required to validate the tag."
-fi
+    msg="# managed by release.sh"
+    sed -E -i "s/^version = .* $msg$/version = \"${version_tag#v}\"  $msg/" "./pyproject.toml"
+    uv sync
+    git add ./pyproject.toml ./uv.lock
 
-version_tag="$1"
-
-if [ -z "$version_tag" ]; then
-    echo "Please provide a tag."
-    echo "Usage: ./release.sh v[X.Y.Z]"
-    exit
-fi
-
-if semver valid "$version_tag" > /dev/null; then
-  echo "Valid SemVer: $version_tag"
-else
-  echo "Invalid SemVer: \"$version_tag\"" >&2
-  echo "Eg: 0.1.1 0.1.1-dev0"
-  exit 1
-fi
-
-# Define your cleanup or final function
-exit_message() {
-    echo "-----------------"
-    echo "If you aren't happy with these changes. try again with"
-    echo "git reset --soft HEAD~1"
-    echo "git tag -d $version_tag"
+    sed -E -i "s/^version: .* $msg$/version: ${version_tag#v}-SET-BY-CICD  $msg/" "./helm/Chart.yaml"
+    git add ./helm/Chart.yaml
 }
-trap exit_message EXIT
 
+export -f release_custom_hook
+export START_COMMIT="ed944248282e099601ce15b4903fa6345c0b7845"
+export RELEASE_CUSTOM_HOOK=release_custom_hook
+export REPO_NAME=toggle-corp/timur-backend
+export DEFAULT_BRANCH=develop
+export VERSION_TAG_PREFIX_MODE=require
 
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$BASE_DIR"
+export GIT_CLIFF__REMOTE__GITHUB__OWNER=toggle-corp
+export GIT_CLIFF__REMOTE__GITHUB__REPO=timur-backend
 
-echo "Preparing $version_tag..."
-# update the version
-msg="# managed by release.sh"
-sed -E -i "s/^version = .* $msg$/version = \"${version_tag#v}\"  $msg/" "./pyproject.toml"
-uv sync
-git add ./pyproject.toml ./uv.lock
-
-sed -E -i "s/^version: .* $msg$/version: ${version_tag#v}-SET-BY-CICD  $msg/" "./helm/Chart.yaml"
-git add ./helm/Chart.yaml
-
-# update the changelog
-git-cliff "$START_COMMIT..HEAD" --config cliff.toml --tag "$version_tag" > CHANGELOG.md
-git add CHANGELOG.md
-git commit -m "chore(release): prepare for $version_tag"
-git show
-
-# generate a changelog for the tag message
-export GIT_CLIFF_TEMPLATE="\
-    {% for group, commits in commits | group_by(attribute=\"group\") %}
-    {{ group | upper_first }}\
-    {% for commit in commits %}
-        - {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
-    {% endfor %}
-    {% endfor %}"
-changelog=$(git-cliff "$START_COMMIT..HEAD" --config detailed.toml --unreleased --strip all)
-
-# create a signed tag
-# https://keyserver.ubuntu.com/pks/lookup?search=0x4A92FA17B6619297&op=vindex
-git tag "$version_tag" -m "Release $version_tag" -m "$changelog"
-git tag -v "$version_tag"
-echo "Done!"
-echo "Now push the commit (git push) and the tag (git push --tags)."
+# Forward the argument - used for pre-fill version
+"$SCRIPT_DIR/fugit/scripts/release.sh" "${@:-}"
