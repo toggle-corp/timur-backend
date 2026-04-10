0b8343631af5e48cc8b21091b825613fd2a94b13
thenav56 <ayernavin@gmail.com>
2026-04-10 13:20:17 +0545

    chore(deps): upgrade fugit to v0.1.6

--- helm/update-snapshots.sh
+++ helm/update-snapshots.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SCRIPT_DIR
 
 # shellcheck disable=SC2068
-"$SCRIPT_DIR/../fugit/scripts/helm-update-snapshots.sh" $@
+"$SCRIPT_DIR/../fugit/scripts/helm-update-snapshots.sh" "$@"
